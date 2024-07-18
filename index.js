dc461a3f339f651a460c17249208270235cc6cae
Yasmin Hillis <jasminehillis94@gmail.com>
2024-07-18 23:38:59 +0300

    add timer, new game button and corresponding media queries

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 let homeScreen = document.getElementById('home-screen');
 let guestScreen = document.getElementById('guest-screen');
 
+let timerScreen = document.getElementById('timer-screen');
+let newGameButton = document.getElementById('new-game');
+
+newGameButton.disabled = true;
+newGameButton.disabled = true;
 var homeScore = 0;
 var guestScore = 0;
 
@@ -18,7 +23,6 @@ function addTwoPointsToHome() {
 }
 
 function addThreePointsToHome() {
-    
     homeScore += 3
     homeScreen.textContent = homeScore
     checkWhoIsLeading()
@@ -55,3 +59,52 @@ function checkWhoIsLeading() {
     }
 }
 
+let time = 10;
+function startTimer() {
+    let timerId = setInterval(function () {
+        if (time > 0) {
+            time--;
+            let minutes = Math.floor(time / 60);
+            let seconds = time % 60;
+
+            if (seconds < 10) {
+                seconds = `0${seconds}`
+            }
+
+            timerScreen.textContent = `${minutes}:${seconds}`
+       
+        } else {
+            clearInterval(timerId)
+            timerScreen.textContent = `Time\'s up`
+            newGameButton.disabled = false;
+            disableIncButtons()
+            
+        }
+    }, 1000)
+}
+
+startTimer()
+
+function disableIncButtons() {
+    let buttons = document.querySelectorAll('.add-points-btn');
+    buttons.forEach(button => button.disabled = true)
+}
+
+function enableIncButtons() {
+    let buttons = document.querySelectorAll('.add-points-btn');
+    buttons.forEach(button => button.disabled = false)
+}
+
+function newGame() {
+    time = 10;
+    homeScore = 0
+    homeScreen.textContent = '0';
+    guestScore = 0;
+    guestScreen.textContent = '0';
+    guestScreen.classList.remove('highlight')
+    homeScreen.classList.remove('highlight');
+    timerScreen.textContent = "00:00"
+    enableIncButtons()
+    startTimer()
+    newGameButton.disabled = true
+}
